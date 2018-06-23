81947ba820cb578d808b1cce078561262a210b5b
kon 4 <poe3@web.de>
2018-06-23 15:04:05 +0200

    Change FileUploader files

--- backend/src/main/java/edu/wallcloud/backend/BackendApplication.java
+++ backend/src/main/java/edu/wallcloud/backend/BackendApplication.java
@@ -3,10 +3,26 @@ package edu.wallcloud.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import edu.wallcloud.backend.storage.StorageProperties;
+import edu.wallcloud.backend.storage.StorageService;
+
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
 	}
 }
